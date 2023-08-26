4ebd33afbedf01d901162e5d7aff614b2e9b5383
Franco Magistrati <francomagistrati@gmail.com>
2023-08-26 15:59:34 -0300

    desafio

--- src/index.js
+++ src/index.js
@@ -1,23 +1,98 @@
-import productRouter from "./routers/product.js";
-import cartRouter from "./routers/cart.js";
-import express  from "express";
-import { __dirname } from "./path.js";
-import path from "path"
+import express from 'express'
+import http from 'http';
+import { Server } from 'socket.io';
+import { engine } from 'express-handlebars';
+import bodyParser from 'body-parser';
+import { __dirname } from './path.js';
+import path from 'path'
 
-const app = express()
-const PORT = 8080
-app.get("/", (req, res) => {
-    res.send("Bienvenido a la API de productos");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+
+app.set('views', path.join(__dirname, 'vista'));
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+function obtenerProductos() {
+
+  const jsonString = fs.readFileSync('../../productos.json', 'utf-8');
+
+
+  const productos = JSON.parse(jsonString);
+
+
+  return productos;
+}
+
+
+function crearProducto(nombre) {
+
+  const producto = nombre;
+
+
+  io.emit('nuevoProducto', producto);
+}
+
+
+function eliminarProducto(id) {
+
+  const productoId = id;
+
+
+  io.emit('eliminarProducto', productoId);
+}
+
+
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado');
+
+
+  socket.on('nuevoProducto', (producto) => {
+
+    io.emit('productoCreado', producto);
   });
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
 
-app.use('/api/products', productRouter)
-app.use('/static', express.static(path.join(__dirname, '/public')))
-app.use('/api/cart', cartRouter);
+  socket.on('eliminarProducto', (productoId) => {
+
+    io.emit('productoEliminado', productoId);
+  });
+});
 
 
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
+app.get('/realtimeproducts', (req, res) => {
 
+  res.render('realTimeProducts', { productos: obtenerProductos() });
+});
+
+
+app.post('/crearproducto', (req, res) => {
+  const nombreProducto = req.body.nombre;
+
+  io.emit('nuevoProducto', nombreProducto);
+
+  res.redirect('/realtimeproducts');
+});
+
+
+app.post('/eliminarproducto', (req, res) => {
+  const productoId = req.body.id;
+
+ 
+  io.emit('eliminarProducto', productoId);
+
+  res.redirect('/realtimeproducts');
+});
+
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
