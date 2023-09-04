ae19ebc9bbd97eec8fab59704c30540452bfe2e4
Franco Magistrati <francomagistrati@gmail.com>
2023-09-04 20:24:54 -0300

    intento de correccion

--- src/index.js
+++ src/index.js
@@ -1,71 +1,73 @@
-import express from 'express'
-import multer from 'multer'
-import prodsRouter from "./routes/products.routes.js";
+import express from 'express';
+import multer from 'multer';
+import prodsRouter from "./routers/product.js";
 import { __dirname } from './path.js';
 import { engine } from 'express-handlebars';
 import { Server } from 'socket.io';
 import path from 'path';
-import { promises as fs } from 'fs'; // Importa módulo "fs"
+import { promises as fs } from 'fs';
 
-const PORT = 4000
-const app = express()
+const PORT = 4000;
+const app = express();
 
-//Config
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'src/public/img') //null hace referencia a que no envia errores
+        cb(null, 'src/public/img');
     },
     filename: (req, file, cb) => {
-        cb(null, `${Date.now()}${file.originalname}`) //Concateno el nombre original de mi archivo con milisegundos con Date.now()
+        cb(null, `${Date.now()}${file.originalname}`);
     }
-})
+});
 
 const serverExpress = app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
+    console.log(`Server on port ${PORT}`);
+});
 
-//Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const io = new Server(serverExpress);
+const prods = [];
 
-app.engine('handlebars', engine()) //Defino que motor de plantillas voy a utilizar y su config
-app.set('view engine', 'handlebars') //Setting de mi app de hbs
-app.set('views', path.resolve(__dirname, './views')) //Resolver rutas absolutas a traves de rutas relativas
-const upload = multer({ storage: storage })
-app.use('/static', express.static(path.join(__dirname, '/public'))) //Unir rutas en una sola concatenandolas
-
-//Server Socket.io
-const io = new Server(serverExpress)
-const prods = []
 io.on('connection', (socket) => {
-    console.log("Servidor Socket.io conectado")
+    console.log("Servidor Socket.io conectado");
 
     socket.on('nuevoProducto', async (nuevoProd) => {
-        prods.push(nuevoProd)
-        socket.emit('prods', prods)
+        prods.push(nuevoProd);
+        io.emit('prods', prods);
 
-        // Aquí puedes insertar tu lógica para escribir a un archivo utilizando fs
         const productosJson = JSON.stringify(prods);
-        await fs.writeFile('productos.json', productosJson); // Guarda la lista de productos en un archivo
-    })
+        await fs.writeFile('productos.json', productosJson);
+    });
+});
 
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Routes
+app.engine('handlebars', engine({ defaultLayout: false }));
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname, './views'));
 
-app.use('/api/products', prodsRouter)
+const upload = multer({ storage: storage });
+app.use('/static', express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('home.handlebars', { title: 'Mi Aplicación' });
+});
+
+app.use('/api/product', prodsRouter);
 
 app.get('/static', (req, res) => {
     res.render('realTimeProducts', {
         css: "style.css",
         title: "Chat",
         js: "realTimeProducts.js"
-
-    })
-})
+    });
+});
 
 app.post('/upload', upload.single('product'), (req, res) => {
-    console.log(req.file)
-    console.log(req.body)
-    res.status(200).send("Imagen cargada")
-})
+    console.log(req.file);
+    console.log(req.body);
+    res.status(200).send("Imagen cargada");
+});
+
+console.log(path.resolve(__dirname, './views'));
+
+export default app;
